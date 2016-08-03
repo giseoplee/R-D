9ffdc517e995d2aa2f777ff8e8b993edf15ee6b3
giseoplee <llgs901@naver.com>
2016-08-03 16:00:07 +0900

    merge complete
    T is C

--- app.js
+++ app.js
@@ -9,20 +9,16 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var compression = require('compression');
-<<<<<<< HEAD
 var os = require('os');
 
-=======
-var os = require('os');  
->>>>>>> origin/master
 var dbService = require("./Service/DBService.js");
 var routesService = require("./Service/RoutesService.js");
 var index = require('./Controller/ViewController.js');
-var config = require('./Config.js'); 
+var config = require('./Config.js');
 global.app = new express();
 
 const httpPort = process.env.PORT || 3000;
-const httpServer = http.createServer(app); 
+const httpServer = http.createServer(app);
 const io = require("socket.io")(httpServer);
 
 var cpuNo = os.cpus().length;
@@ -32,58 +28,26 @@ var cpuNo = os.cpus().length;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-var httpServer = http.createServer(app).listen(8080, function(req,res){
-  console.log('Socket IO server has been started');
-});
-
-var io = require('socket.io').listen(httpServer);
-
 io.on("connection", function (socket) {
-
-  socket.on("new user",function(data){
-      console.log("join",data);
-      socket.join(data.room_id);
-  });
-
-  socket.on("new msg",function(data){
-      console.log("socket : ",socket);
-      //db저장 후
-      console.log("msg",data);
-      io.in(data.room_id).emit("new msg",data);
-  });
-
-// 연결 해제
-  socket.on('disconnect', (data) => {
-      console.log("disconnect");
-      socket.leave();
-  });
-});
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
-
-io.on("connection", function (socket) {  
   socket.on("new user",function(data){
     console.log("join",data);
-    socket.join(data.room_id); 
+    socket.join(data.room_id);
   });
   socket.on("new msg",function(data){
     console.log("socket : ",socket);
-    //db저장 후 
+    //db저장 후
     console.log("msg",data);
     io.in(data.room_id).emit("new msg",data);
-  }) 
+  })
 
   // 연결 해제
-  socket.on('disconnect', (data) => {   
+  socket.on('disconnect', (data) => {
     console.log("disconnect");
-    socket.leave(); 
+    socket.leave();
   });
-})
+});
 
-
-app.use('/', index);  
+app.use('/', index);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -95,13 +59,12 @@ app.use(express.static(path.join(__dirname)));
 app.get('/*', function(req,res,next){
   res.render("index",{title:"Express"});
 });
+
 dbService.Init();
 routesService.Init();
 
- 
 httpServer.listen(httpPort, () => {
   console.log('HTTP server listening on port %d !!!', httpPort);
 });
-
 
 module.exports = app;
