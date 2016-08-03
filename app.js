e7b75e225feaa11ea963184205e67f13d849fd14
HyunWoo <xodltus@naver.com>
2016-08-03 14:51:50 +0900

    route

--- app.js
+++ app.js
@@ -33,8 +33,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname)));
 dbService.Init();
 routesService.Init();
 
