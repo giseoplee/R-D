061e1781b64b4f8e86572c3c483bee2e72b92c0d
HyunWoo <xodltus@naver.com>
2016-08-04 17:47:05 +0900

    ls

--- Controller/SocketController.js
+++ Controller/SocketController.js
@@ -1,49 +1,51 @@
 var util = require('util');
 var surveyModel = require('../Model/SurveyModel.js');
-
+var async = require("async");
 var SocketService = function () {
 };
 
-SocketService.Init = function() {
+SocketService.Init = function () {
 
-  io.on("connection", function (socket) {
+    io.on("connection", function (socket) {
 
-    socket.on("new user",function(data){
-        console.log("join",data);
-        socket.join(data.room_id);
-    });
+        socket.on("new user", function (data) {
+            console.log("join", data);
+            socket.join(data.room_id);
+        });
 
-    socket.on("new msg",function(data){
-        // console.log("socket : ",socket);
+        socket.on("new msg", function (data) {
+            // console.log("socket : ",socket);
+
+            var surveyId = data.survey;
+            var updateItemIndex = data.index;
+
+            if (surveyId === undefined || updateItemIndex === undefined) {
+                throw { code: errorCode.ParamError };
+            }
+
+            surveyModel.updateSurveyItem(surveyId, updateItemIndex, function (result) {
+
+                surveyModel.findSurveyDetail(surveyId, function (result) {
+
+                    var array = [];
  
-        var surveyId = data.survey;
-        var updateItemIndex = data.index;
-
-        if (surveyId === undefined || updateItemIndex === undefined) {
-            throw { code: errorCode.ParamError };
-        }
-
-        surveyModel.updateSurveyItem(surveyId, updateItemIndex, function (result) {
-
-            surveyModel.findSurveyDetail(surveyId, function (result){
-
-                var array = [];
-
-                for (var i = 0; i < result.length; i++) {
-                    array.push(result[i]);
-                } 
-                socket.broadcast.to(data.room_id).emit("new msg",array); 
-                socket.emit("new msg",array);
+                    async.eachSeries(result, (data, callback) => {
+                        array.push(data)
+                        callback(array);
+                    }, (array) => {
+                        socket.broadcast.to(data.room_id).emit("new msg", array);
+                        socket.emit("new msg", array); 
+                    });
+                });
             });
         });
-    });
 
-// 연결 해제
-    socket.on('disconnect', function(data){
-        console.log("disconnect");
-        socket.leave();
+        // 연결 해제
+        socket.on('disconnect', function (data) {
+            console.log("disconnect");
+            socket.leave();
+        });
     });
-  });
 }
 
 module.exports = SocketService;
